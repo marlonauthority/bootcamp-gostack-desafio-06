cab1df4db92bff525d9162911a906443a80c6a0f
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-01 22:24:04 -0400

    react navigation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { View } from 'react-native';
 import './config/ReactotronConfig';
+import Routes from './routes';
 
 const App = () => {
-  return <></>;
+  return <Routes />;
 };
 
 export default App;
