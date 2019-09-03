fc2f15314b1aa740873a5778e66b6d68f9264630
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-03 00:40:15 -0400

    pagination refreshing

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -34,6 +34,7 @@ export default class User extends Component {
     stars: [],
     loading: true,
     page: 1,
+    refreshing: false,
   };
 
   async componentDidMount() {
@@ -48,21 +49,20 @@ export default class User extends Component {
   }
 
   loadData = async () => {
-    try {
-      const { navigation } = this.props;
-      const { stars, page } = this.state;
-      const user = navigation.getParam('user');
+    const { navigation } = this.props;
+    const { stars, page } = this.state;
+    const user = navigation.getParam('user');
 
-      const response = await api.get(`/users/${user.login}/starred`, {
-        params: { page },
-      });
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: { page },
+    });
 
-      this.setState({
-        stars: page >= 2 ? [...stars, ...response.data] : response.data,
-        page,
-        loading: false,
-      });
-    } catch (err) {}
+    this.setState({
+      stars: page >= 2 ? [...stars, ...response.data] : response.data,
+      page,
+      loading: false,
+      refreshing: false,
+    });
   };
 
   loadMore = () => {
@@ -71,9 +71,18 @@ export default class User extends Component {
     this.setState({ page: nextPage });
   };
 
+  refreshList = () => {
+    const { page, stars } = this.state;
+    this.setState({
+      stars: page === 1 ? [...stars] : [],
+      refreshing: page >= 2 && true,
+      page: 1,
+    });
+  };
+
   render() {
     const { navigation } = this.props;
-    const { stars, loading } = this.state;
+    const { stars, loading, refreshing } = this.state;
     const user = navigation.getParam('user');
     return (
       <Container>
@@ -89,6 +98,8 @@ export default class User extends Component {
           <Stars
             data={stars}
             keyExtractor={star => String(star.id)}
+            onRefresh={this.refreshList} // Função dispara quando o usuário arrasta
+            refreshing={refreshing} // Variável que armazena um estado tr
             onEndReachedThreshold={0.2} // Carrega mais itens quando chegar em 20% d
             onEndReached={this.loadMore} // Função que carrega mais itens
             renderItem={({ item }) => (
